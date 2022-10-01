4a0c35e95db119d93615d7ea6ef258b9bdd9bbd0
richard rosenblat <rosenblatr@gmail.com>
2022-09-30 21:33:45 -0300

    filtrando eventos

--- src/state/hooks/useListaDeEvento.ts
+++ src/state/hooks/useListaDeEvento.ts
@@ -1,6 +1,7 @@
 import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../atom';
+import eventosFiltradosState from '../seletores/index';
+
 const useListaDeEventos = ()=>{
-    return useRecoilValue(listaDeEventosState)
+    return useRecoilValue(eventosFiltradosState)
 }
 export default useListaDeEventos
